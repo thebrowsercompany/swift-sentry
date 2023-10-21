877464e71d2f8a9c6f702b1fa7d4fe06b4f7d984
Brian Michel <brian.michel@gmail.com>
2023-10-21 09:11:41 -0400

    Fix macOS building from Xcode

---
+++ Sources/sentry/dummy.c
@@ -0,0 +1,1 @@
+// A required, empty file to ensure that when building from Xcode that a .o is produced.
--- Tests/SwiftSentryTests/SwiftSentryTests.swift
+++ Tests/SwiftSentryTests/SwiftSentryTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import swift_sentry
+@testable import SwiftSentry
 
-final class swift_sentryTests: XCTestCase {
+final class SwiftSentryTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
